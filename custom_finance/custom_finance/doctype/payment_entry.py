a9dbaedd66edf61db92678dda54fb6c4b9a40349
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-04-26 11:38:10 +0530

    Ouststanding Button

--- custom_finance/custom_finance/doctype/payment_entry.py
+++ custom_finance/custom_finance/doctype/payment_entry.py
@@ -3,6 +3,7 @@ import json
 import frappe
 from frappe import ValidationError, _, scrub, throw
 from frappe.utils import cint, comma_or, flt, getdate, nowdate
+from numpy import append
 from six import iteritems, string_types
 
 import erpnext
@@ -1152,9 +1153,10 @@ def validate_inclusive_tax(tax, doc):
 		elif tax.get("category") == "Valuation":
 			frappe.throw(_("Valuation type charges can not be marked as Inclusive"))
 
+
+###################################################
 @frappe.whitelist()
 def get_outstanding_reference_documents(args):
-
 	if isinstance(args, string_types):
 		args = json.loads(args)
 
@@ -1234,6 +1236,130 @@ def get_outstanding_reference_documents(args):
 
 	return data
 
+################################################################
+
+@frappe.whitelist()
+def get_outstanding_fees(args):
+	if isinstance(args, string_types):
+		args = json.loads(args)
+	################ Fee Component
+	filter=[]
+
+	filter.append(["Student","=",args.get('party')])
+	filter.append(['posting_date', 'between',[args.get('from_posting_date'),args.get('to_posting_date')]])
+	filter.append(["outstanding_amount",">",0])	
+
+	if args.get('outstanding_amt_greater_than') > 0:
+		filter.append(["outstanding_amount",">",args.get('outstanding_amt_greater_than')])
+	if args.get('outstanding_amt_less_than') >0:
+		filter.append(["outstanding_amount","<",args.get('outstanding_amt_less_than')])	
+	if args.get('cost_center'):
+		filter.append(['cost_center',"=",args.get('cost_center')])	
+	
+	if args.get('from_due_date') and args.get('to_due_date'):
+		filter.append(['valid From','between',[args.get('from_due_date'),args.get('to_due_date')]])
+		filter.append(['valid_to','between',[args.get('from_due_date'),args.get('to_due_date')]])
+
+	fees_info=frappe.db.get_all("Fees",filter,['name','posting_date'])
+	######################### end fees
+	fee_component_info=[]
+	for t in fees_info:
+		fee_component=frappe.db.get_all("Fee Component", {"parent":t['name']},
+									["name","fees_category","outstanding_fees","receivable_account","income_account","outstanding_fees","amount"])
+		if fee_component[0]["outstanding_fees"]	>0:			
+			fee_component[0]['posting_date']=t['posting_date']	
+			fee_component[0]['Type']='Fees'
+			fee_component[0]['reference_name']=t['name']	
+			fee_component_info.append(fee_component[0])
+	data=fee_component_info
+	# frappe.db.get_all("Fee Component", {"parent":})
+
+	# if args.get('party_type') == 'Member':
+	# 	return
+
+	# # confirm that Supplier is not blocked
+	# if args.get('party_type') == 'Supplier':
+	# 	supplier_status = get_supplier_block_status(args['party'])
+	# 	if supplier_status['on_hold']:
+	# 		if supplier_status['hold_type'] == 'All':
+	# 			return []
+	# 		elif supplier_status['hold_type'] == 'Payments':
+	# 			if not supplier_status['release_date'] or getdate(nowdate()) <= supplier_status['release_date']:
+	# 				return []
+
+	# party_account_currency = get_account_currency(args.get("party_account"))
+	# company_currency = frappe.get_cached_value('Company',  args.get("company"),  "default_currency")
+
+	# # Get positive outstanding sales /purchase invoices/ Fees
+	# condition = ""
+	# if args.get("voucher_type") and args.get("voucher_no"):
+	# 	condition = " and voucher_type={0} and voucher_no={1}"\
+	# 		.format(frappe.db.escape(args["voucher_type"]), frappe.db.escape(args["voucher_no"]))
+
+	# # Add cost center condition
+	# if args.get("cost_center"):
+	# 	condition += " and cost_center='%s'" % args.get("cost_center")
+
+	# date_fields_dict = {
+	# 	'posting_date': ['from_posting_date', 'to_posting_date'],
+	# 	'due_date': ['from_due_date', 'to_due_date']
+	# }
+
+	# for fieldname, date_fields in date_fields_dict.items():
+	# 	if args.get(date_fields[0]) and args.get(date_fields[1]):
+	# 		condition += " and {0} between '{1}' and '{2}'".format(fieldname,
+	# 			args.get(date_fields[0]), args.get(date_fields[1]))
+
+	# if args.get("company"):
+	# 	condition += " and company = {0}".format(frappe.db.escape(args.get("company")))
+
+	# outstanding_invoices = get_outstanding_invoices(args.get("party_type"), args.get("party"),
+	# 	args.get("party_account"), filters=args, condition=condition)
+
+	# outstanding_invoices = split_invoices_based_on_payment_terms(outstanding_invoices)
+
+	# for d in outstanding_invoices:
+	# 	d["exchange_rate"] = 1
+	# 	if party_account_currency != company_currency:
+	# 		if d.voucher_type in ("Sales Invoice", "Purchase Invoice", "Expense Claim"):
+	# 			d["exchange_rate"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "conversion_rate")
+	# 		elif d.voucher_type == "Journal Entry":
+	# 			d["exchange_rate"] = get_exchange_rate(
+	# 				party_account_currency,	company_currency, d.posting_date
+	# 			)
+	# 	if d.voucher_type in ("Purchase Invoice"):
+	# 		d["bill_no"] = frappe.db.get_value(d.voucher_type, d.voucher_no, "bill_no")
+
+	# # Get all SO / PO which are not fully billed or aginst which full advance not paid
+	# orders_to_be_billed = []
+	# if (args.get("party_type") != "Student"):
+	# 	orders_to_be_billed =  get_orders_to_be_billed(args.get("posting_date"),args.get("party_type"),
+	# 		args.get("party"), args.get("company"), party_account_currency, company_currency, filters=args)
+
+	# # Get negative outstanding sales /purchase invoices
+	# negative_outstanding_invoices = []
+	# if args.get("party_type") not in ["Student", "Employee"] and not args.get("voucher_no"):
+	# 	negative_outstanding_invoices = get_negative_outstanding_invoices(args.get("party_type"), args.get("party"),
+	# 		args.get("party_account"), party_account_currency, company_currency, condition=condition)
+
+	# data = negative_outstanding_invoices + outstanding_invoices + orders_to_be_billed
+
+
+	# data=""
+
+	if not data:
+		frappe.msgprint(_("No outstanding invoices found for the {0} {1} which qualify the filters you have specified.")
+			.format(_(args.get("party_type")).lower(), frappe.bold(args.get("party"))))
+
+	return data
+
+
+
+
+
+
+
+#################################################################################
 
 def split_invoices_based_on_payment_terms(outstanding_invoices):
 	invoice_ref_based_on_payment_terms = {}
