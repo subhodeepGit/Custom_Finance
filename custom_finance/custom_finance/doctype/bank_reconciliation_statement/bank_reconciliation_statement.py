fd2ec5e4d84cc51ed4da8a42d0f78a69c34574de
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-05-30 13:04:20 +0530

    Student bank details upload

--- custom_finance/custom_finance/doctype/bank_reconciliation_statement/bank_reconciliation_statement.py
+++ custom_finance/custom_finance/doctype/bank_reconciliation_statement/bank_reconciliation_statement.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class BankReconciliationStatement(Document):
 	def validate(doc):
 		doc.total_allocated_amount=doc.amount
+	def on_submit(doc):
+		st_payment_upload=frappe.db.get_all("Payment Details Upload",{'docstatus':1,'unique_transaction_reference_utr':doc.unique_transaction_reference_utr,
+																	"type_of_transaction":doc.type_of_transaction},['name'])
+
+		print("\n\n\n\n")
+		print(st_payment_upload)
--- custom_finance/custom_finance/doctype/payment_details_upload/payment_details_upload.py
+++ custom_finance/custom_finance/doctype/payment_details_upload/payment_details_upload.py
@@ -1,8 +1,35 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import date 
+import pandas as pd
 
 class PaymentDetailsUpload(Document):
-	pass
+	def validate(self):
+		today = date.today()
+		date_of_transaction=pd.to_datetime(pd.to_datetime(self.date_of_transaction).date())
+		if date_of_transaction<=today:
+			pass
+		else:
+			frappe.throw("Posting date can't in future")
+	def on_submit(self):
+		brs_info=frappe.db.get_all("Bank Reconciliation Statement",{'docstatus':1,'unique_transaction_reference_utr':self.unique_transaction_reference_utr,
+																	"type_of_transaction":self.type_of_transaction},['name','amount','date'])
+		if not brs_info:
+			self.reconciliation_status=0
+		else:
+			if self.amount==brs_info[0]['amount']:	
+				self.reconciliation_status=1
+				self.brs_name=brs_info[0]['name']
+				self.date_of_transaction=brs_info[0]['date']
+				frappe.db.set_value("Payment Details Upload",self.name,'reconciliation_status',1)
+				frappe.db.set_value("Payment Details Upload",self.name,'brs_name',brs_info[0]['name'])
+				frappe.db.set_value("Payment Details Upload",self.name,'date_of_transaction',brs_info[0]['date'])
+			else:
+				self.reconciliation_status=0
+				self.brs_name=''
+				frappe.db.set_value("Payment Details Upload",self.name,'reconciliation_status',0)
+				frappe.db.set_value("Payment Details Upload",self.name,'brs_name','')
+				frappe.msgprint("paid amount does not match with Bank Data")	
