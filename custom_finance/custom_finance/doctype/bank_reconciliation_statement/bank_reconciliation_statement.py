e0679caa59e85ad033c4eed1868124da6bd9d785
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-05-30 22:55:53 +0530

    bank recon withsudent upload

--- custom_finance/custom_finance/doctype/bank_reconciliation_statement/bank_reconciliation_statement.py
+++ custom_finance/custom_finance/doctype/bank_reconciliation_statement/bank_reconciliation_statement.py
@@ -9,7 +9,10 @@ class BankReconciliationStatement(Document):
 		doc.total_allocated_amount=doc.amount
 	def on_submit(doc):
 		st_payment_upload=frappe.db.get_all("Payment Details Upload",{'docstatus':1,'unique_transaction_reference_utr':doc.unique_transaction_reference_utr,
-																	"type_of_transaction":doc.type_of_transaction},['name'])
-
-		print("\n\n\n\n")
-		print(st_payment_upload)
+																	"type_of_transaction":doc.type_of_transaction},['name','amount'])
+		if st_payment_upload[0]['amount']==doc.amount:
+			frappe.db.set_value("Payment Details Upload",st_payment_upload[0]['name'],'reconciliation_status',1)
+			frappe.db.set_value("Payment Details Upload",st_payment_upload[0]['name'],'brs_name',doc.name)
+			frappe.db.set_value("Payment Details Upload",st_payment_upload[0]['name'],'date_of_transaction',doc.date)
+		else:
+			frappe.msgprint("Not Reconciled But data saved")	
