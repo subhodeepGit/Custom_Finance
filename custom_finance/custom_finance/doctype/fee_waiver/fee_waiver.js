0a7cf5d277f3988b8d287880d7848b9a02a92772
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-06-03 15:38:47 +0530

    Bug fix in fee wavier and it's report

--- custom_finance/custom_finance/doctype/fee_waiver/fee_waiver.js
+++ custom_finance/custom_finance/doctype/fee_waiver/fee_waiver.js
@@ -303,7 +303,7 @@ frappe.ui.form.on("Fee Waiver Components", "waiver_amount", function(frm, cdt, c
 		}
         refresh_field("amount", d.name, d.parentfield);
         refresh_field("total_waiver_amount", d.name, d.parentfield);
-		if(d.waiver_amount > d.outstanding_fees_ref){
+		if(d.waiver_amount > d.grand_fee_amount){
 			d.waiver_amount=0
 			d.total_waiver_amount=0
 			d.outstanding_fees=0
