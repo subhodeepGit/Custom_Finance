31bc883009e28d9c711143dcec720a51b8bd13a6
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-06-28 15:15:23 +0530

    custom button added in brs

--- custom_finance/custom_finance/doctype/bank_reconciliation_statement/bank_reconciliation_statement.js
+++ custom_finance/custom_finance/doctype/bank_reconciliation_statement/bank_reconciliation_statement.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Bank Reconciliation Statement', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.docstatus==1){
+			frm.add_custom_button(__('View Payment Entry Records'), function() {
+				frappe.route_options = {
+					reference_no: frm.doc.unique_transaction_reference_utr
+				};
+				frappe.set_route('List', 'Payment Entry');
+			});
+		}
+		if (frm.doc.docstatus==1){
+			frm.add_custom_button(__('View Surplus Payment Entry Records'), function() {
+				frappe.route_options = {
+					reference_no: frm.doc.unique_transaction_reference_utr
+				};
+				frappe.set_route('List', 'Payment Refund');
+			});
+		}
+	}
 });
